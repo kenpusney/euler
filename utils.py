70a1864bb19937c135b63e3278847b58618c3de3
Kimmy Leo <kenpusney@gmail.com>
2017-07-24 02:38:51 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -79,3 +79,8 @@ def rcollect(m, i, j, size):
         j -= 1
         if j < 0:
             break
+
+def factors(n):
+    from functools import reduce
+    return set(reduce(list.__add__, 
+                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
