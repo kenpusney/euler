6117a9fe5b7892443d3887b832c950d722cf490f
Kimmy Leo <kenpusney@gmail.com>
2017-07-24 02:54:21 +0800

    add factorial func

--- utils.py
+++ utils.py
@@ -84,3 +84,9 @@ def factors(n):
     from functools import reduce
     return set(reduce(list.__add__, 
                 ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
+def fact(n):
+    prod = 1
+    for i in range(1, n+1):
+        prod *= i
+    return prod
